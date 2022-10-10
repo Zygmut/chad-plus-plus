d66c94997671012be8f1972d071c9e8bf887c786
unknown <sergi2808@gmail.com>
2022-10-10 20:58:58 +0200

    Update Chadpp.java

--- src/main/java/compiladores/chadpp/Chadpp.java
+++ src/main/java/compiladores/chadpp/Chadpp.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package compiladores.chadpp;
 
 /**
  *
- * @author Sergi
+ * Main class of the project
  */
 public class Chadpp {
 
