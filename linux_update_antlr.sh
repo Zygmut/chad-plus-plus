b775571fd0bad7e8d61a66720382ae2cc0ab66c5
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2022-10-10 22:55:38 +0200

    Brawling with ANTLR4 2

---
+++ linux_update_antlr.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+java -jar antlr-4.9.3-complete.jar src/main/java/compiladores/chadpp/compiler_components/AritmeticLexer.g4
+java -jar antlr-4.9.3-complete.jar -visitor src/main/java/compiladores/chadpp/compiler_components/AritmeticParser.g4
---
+++ windows_update_antlr.bat
@@ -0,0 +1,2 @@
+java -jar antlr-4.9.3-complete.jar src/main/java/compiladores/chadpp/compiler_components/AritmeticLexer.g4
+java -jar antlr-4.9.3-complete.jar -visitor src/main/java/compiladores/chadpp/compiler_components/AritmeticParser.g4
