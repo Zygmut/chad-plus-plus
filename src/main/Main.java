f3374f2cc2da47ea393679e3049a38f4a20357d3
Sergi Mayol <sergi2808@gmail.com>
2022-10-11 09:11:42 +0200

    reestructuración proyecto

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import org.antlr.runtime.Token;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Token.SKIP_TOKEN);
+        System.out.println("Hello World!");
+    }
+
+}
