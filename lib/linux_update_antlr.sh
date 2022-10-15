7343a24d1d368e3c0d7c1404338cb0831691d28f
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2022-10-15 12:10:40 +0200

    Format linux update

--- lib/linux_update_antlr.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-java -jar antlr-4.9.3-complete.jar ../grammar/AritmeticLexer.g4 -o ../src/antlr
-java -jar antlr-4.9.3-complete.jar -visitor ../grammar/AritmeticParser.g4 -o ../src/antlr
