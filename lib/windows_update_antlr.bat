9ec1d4b46ce604fb6b3906ef6b901895830aabbf
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2022-10-23 00:21:17 +0200

    Changed scripts to work from root
    Co-authored-by: Sergi Mayol <Sergimayol@users.noreply.github.com>

--- lib/windows_update_antlr.bat
+++ lib/windows_update_antlr.bat
@@ -1,2 +1,2 @@
-java -jar antlr-4.9.3-complete.jar ../grammar/ChadppLexer.g4 -o ../src/antlr
-java -jar antlr-4.9.3-complete.jar -visitor ../grammar/ChadppParser.g4 -o ../src/antlr
+java -jar lib/antlr-4.9.3-complete.jar grammar/ChadppLexer.g4 -o src/antlr
+java -jar lib/antlr-4.9.3-complete.jar -visitor grammar/ChadppParser.g4 -o src/antlr
