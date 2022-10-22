4da9f4ec0ebd4d29d596096002a0a800503f1ddf
unknown <sergi2808@gmail.com>
2022-10-22 10:57:15 +0200

    Update windows_update_antlr.bat

--- lib/windows_update_antlr.bat
+++ lib/windows_update_antlr.bat
@@ -1,2 +1,2 @@
-java -jar antlr-4.9.3-complete.jar ../grammar/AritmeticLexer.g4 -o ../src/antlr
-java -jar antlr-4.9.3-complete.jar -visitor ../grammar/AritmeticParser.g4 -o ../src/antlr
+java -jar antlr-4.9.3-complete.jar ../grammar/ChadppLexer.g4 -o ../src/antlr
+java -jar antlr-4.9.3-complete.jar -visitor ../grammar/ChadppParser.g4 -o ../src/antlr
