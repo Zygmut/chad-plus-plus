568826e387775b5746cafa73e5f643181a650dbd
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2022-10-23 00:05:03 +0200

    Environment
    Co-authored-by: Sergi Mayol <Sergimayol@users.noreply.github.com>

--- src/utils/Env.java
+++ src/utils/Env.java
@@ -5,16 +5,20 @@ package utils;
  *
  * En esta clase se definen las variables de entorno y las constantes que se
  * emplearan a lo largo del programa.
- * 
+ *
  * Assignatura 21742 - Compiladors
  * Estudis: Grau en Informàtica
  * Itinerari: Computació
  * Curs: 2022 - 2023
- * 
+ *
  */
 public class Env {
-    // Test mode
+    // Test mode (Unit tests)
     public static final boolean TEST_MODE = true;
+
+    // Debug mode
+    public static final boolean DEBUG_MODE = false;
+
     // Constante que indica si se desea visualizar el arbol de parseo
     public static final boolean VISUALIZATION = false;
 
@@ -26,6 +30,6 @@ public class Env {
 
     // Constantes para el manejo de errores y excepciones en el input del programa
     public static final int Ok = 0;
-    public static final int Error = 0;
-    public static final int Warning = 1;
+    public static final int Error = -2;
+    public static final int Warning = -1;
 }
