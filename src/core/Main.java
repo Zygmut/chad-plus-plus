3f5ee3998082560445b3d78be8a7a8a6a137c250
unknown <sergi2808@gmail.com>
2022-10-23 16:39:15 +0200

    Core/Main :white_check_mark:

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,5 +1,55 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Main {
+
+    private List<Declaration> listOfDeclaraions;
+    private List<Instruction> lisfOfInstructions;
+
+    public Main() {
+        this.listOfDeclaraions = new ArrayList<Declaration>();
+        this.lisfOfInstructions = new ArrayList<Instruction>();
+    }
+
+    public Main(List<Declaration> listOfDeclaraions, List<Instruction> lisfOfInstructions) {
+        this.listOfDeclaraions = listOfDeclaraions;
+        this.lisfOfInstructions = lisfOfInstructions;
+    }
+
+    public List<Declaration> getListOfDeclaraions() {
+        return this.listOfDeclaraions;
+    }
+
+    public void setListOfDeclaraions(List<Declaration> listOfDeclaraions) {
+        this.listOfDeclaraions = listOfDeclaraions;
+    }
+
+    public List<Instruction> getLisfOfInstructions() {
+        return this.lisfOfInstructions;
+    }
+
+    public void setLisfOfInstructions(List<Instruction> lisfOfInstructions) {
+        this.lisfOfInstructions = lisfOfInstructions;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Main)) {
+            return false;
+        }
+        Main main = (Main) o;
+        return Objects.equals(listOfDeclaraions, main.listOfDeclaraions)
+                && Objects.equals(lisfOfInstructions, main.lisfOfInstructions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(listOfDeclaraions, lisfOfInstructions);
+    }
 
 }
