dd85f2eab6dcf8ce5b103006d9f9c676628449a4
unknown <sergi2808@gmail.com>
2022-10-23 16:38:54 +0200

    Create Chadpp class

---
+++ src/core/Chadpp.java
@@ -0,0 +1,52 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Chadpp
+ */
+public class Chadpp {
+
+    private List<Function> listOfFunctions;
+    private Main main;
+
+    public Chadpp() {
+        this.listOfFunctions = new ArrayList<>();
+        this.main = new Main();
+    }
+
+    public List<Function> getListOfFunctions() {
+        return this.listOfFunctions;
+    }
+
+    public void setListOfFunctions(List<Function> listOfFunctions) {
+        this.listOfFunctions = listOfFunctions;
+    }
+
+    public Main getMain() {
+        return this.main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Chadpp)) {
+            return false;
+        }
+        Chadpp chadpp = (Chadpp) o;
+        return Objects.equals(listOfFunctions, chadpp.listOfFunctions) && Objects.equals(main, chadpp.main);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(listOfFunctions, main);
+    }
+
+}
