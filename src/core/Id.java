28939dc306c000c7fa16b38a7ec081792ebfc6a2
Zygmut <40397192+Zygmut@users.noreply.github.com>
2022-11-02 11:03:17 +0100

    Id Implementation #9

--- src/core/Id.java
+++ src/core/Id.java
@@ -1,5 +1,47 @@
 package core;
 
+import java.util.Objects;
+
 public class Id {
+
+    private String value;
+
+    public Id() {
+    }
+
+    public Id(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Id)) {
+            return false;
+        }
+        Id id = (Id) o;
+        return Objects.equals(value, id.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " value='" + getValue() + "'" +
+                "}";
+    }
 
 }
