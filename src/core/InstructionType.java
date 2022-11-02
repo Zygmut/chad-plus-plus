8c186f69387370d13d1bba3f02b764c2e93792cb
Zygmut <40397192+Zygmut@users.noreply.github.com>
2022-11-02 12:48:22 +0100

    Added instrucction type by context. needs reviewing

--- src/core/InstructionType.java
+++ src/core/InstructionType.java
@@ -5,4 +5,8 @@ public class InstructionType {
      * Definir los diferentes tipos de instrcciones de los cuales disponemos.
      * Posiblemente en vez de una clase deba ser una enum.
      */
+
+    public enum TypeEnum {
+        IW, IL, II, IR, IC, IO, IA
+    }
 }
