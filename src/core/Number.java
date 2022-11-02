fa1db3b26bcba0cb32eb2c4fee89bde7e7384a1c
Zygmut <40397192+Zygmut@users.noreply.github.com>
2022-11-02 11:00:07 +0100

    Integer (Number) implementation #6

--- src/core/Number.java
+++ src/core/Number.java
@@ -1,5 +1,51 @@
 package core;
 
+import java.util.Objects;
+
 public class Number {
+
+    private Integer value;
+
+    public Number() {
+    }
+
+    public Number(Integer value) {
+        this.value = value;
+    }
+
+    public Number(String value) {
+        this.value = Integer.parseInt(value);
+    }
+
+    public Integer getValue() {
+        return this.value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Number)) {
+            return false;
+        }
+        Number number = (Number) o;
+        return Objects.equals(value, number.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " value='" + getValue() + "'" +
+                "}";
+    }
 
 }
