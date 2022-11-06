47c8f63b5cb03cb71ce3571de9db0c67054c3480
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2022-11-06 14:33:10 +0100

    Added Error codes
    Co-authored-by: Sergi Mayol <Sergimayol@users.noreply.github.com>

---
+++ src/errors/ErrorCodes.java
@@ -0,0 +1,20 @@
+package errors;
+
+/**
+ * ErrorCodes - Clase que contiene los códigos de error de la aplicación.
+ * Se utiliza para identificar el tipo de error que se ha producido en la
+ * compilación y evitar los 'magic numbers'.
+ *
+ * Assignatura 21742 - Compiladors
+ * Estudis: Grau en Informàtica
+ * Itinerari: Computació
+ * Curs: 2022 - 2023
+ *
+ */
+public class ErrorCodes {
+
+    public static final int ARGUMENT_FILE_NOT_FOUND = 0;
+    public static final int ARGUMENT_INVALID_FILE_EXTENSION = 1;
+    public static final int ARGUMENT_INVALID_FILE = 2;
+
+}
