b32a305dc00aa3398cedd22626a3e3271824d21f
Zygmut <40397192+Zygmut@users.noreply.github.com>
2022-11-10 12:59:33 +0100

    Update main tamplate removed post

--- src/main/MainChpp.java
+++ src/main/MainChpp.java
@@ -4,6 +4,16 @@ import errors.ErrorHandler;
 import tests.FileDataTests;
 import utils.Env;
 import utils.Sanity;
+import java.io.CharArrayReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.BufferedReader;
+import java_cup.runtime.ComplexSymbolFactory;
+import java_cup.runtime.Symbol;
+import java_cup.runtime.SymbolFactory;
+import grammar.Scanner;
+import grammar.Parser;
 
 /**
  * Main - Clase principal del programa
@@ -20,13 +30,33 @@ public class MainChpp {
 
     public static void main(String[] args) {
         // Unit Testing
-        if (Env.TEST_MODE) {
-            new FileDataTests().runTests();
-        }
+        // if (Env.TEST_MODE) {
+        // new FileDataTests().runTests();
+        // }
+        //
+        // if (Sanity.checkInput(args) == Env.Error) {
+        // ErrorHandler.printErrors();
+        // System.exit(0);
+        // }
+        Reader input;
 
-        if (Sanity.checkInput(args) == Env.Error) {
+        try {
+            if (args.length > 0) {
+                input = new FileReader(args[0]);
+            } else {
+                System.out.println("Escriu l'expressió que vols calcular:");
+                System.out.print(">>> ");
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                input = new CharArrayReader(in.readLine().toCharArray());
+            }
+
+            SymbolFactory sf = new ComplexSymbolFactory();
+            Scanner scanner = new Scanner(input);
+            Parser parser = new Parser(scanner, sf);
+            Symbol result = parser.parse();
+            System.out.println("Resultat: " + result.value);
+        } catch (Exception e) {
             ErrorHandler.printErrors();
-            System.exit(0);
         }
 
     }
