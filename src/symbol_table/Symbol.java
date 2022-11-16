8454864b87227d47676384128c1c91c6ca462085
Zygmut <40397192+Zygmut@users.noreply.github.com>
2022-11-16 12:31:17 +0100

    Basic structure for symbol table

---
+++ src/symbol_table/Symbol.java
@@ -0,0 +1,5 @@
+package symbol_table;
+
+public class Symbol {
+
+}
---
+++ src/symbol_table/SymbolTable.java
@@ -0,0 +1,24 @@
+package symbol_table;
+
+public class SymbolTable {
+
+    private int n;
+    // public scopeTable st;
+    // public expansionTable et;
+    // public descriptionTable dt;
+
+    public void empty() {
+    }
+
+    public void add(Symbol s) {
+    }
+
+    public void find() {
+    }
+
+    public void enter() {
+    }
+
+    public void exit() {
+    }
+}
