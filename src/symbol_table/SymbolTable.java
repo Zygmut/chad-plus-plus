feafacb5e261f05223704aee8ae42907cbebb8a4
unknown <sergi2808@gmail.com>
2022-11-20 16:56:34 +0100

    Modificación tabla de símbolos (#24)
    Adición propiedad lista de tabla de símbolos hijo

--- src/symbol_table/SymbolTable.java
+++ src/symbol_table/SymbolTable.java
@@ -1,13 +1,37 @@
 package symbol_table;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class SymbolTable {
 
     private HashMap<String, Symbol> SymbolTable;
+    private ArrayList<SymbolTable> children;
 
     public SymbolTable() {
         this.SymbolTable = new HashMap<>();
+        this.children = null;
+    }
+
+    public SymbolTable(HashMap<String, Symbol> symbolTable, ArrayList<SymbolTable> children) {
+        SymbolTable = symbolTable;
+        this.children = children;
+    }
+
+    public HashMap<String, Symbol> getSymbolTable() {
+        return SymbolTable;
+    }
+
+    public void setSymbolTable(HashMap<String, Symbol> symbolTable) {
+        SymbolTable = symbolTable;
+    }
+
+    public ArrayList<SymbolTable> getChildren() {
+        return children;
+    }
+
+    public void setChildren(ArrayList<SymbolTable> children) {
+        this.children = children;
     }
 
     public void addSymbol(String name, Symbol type) {
