37d960640dd2e77d30d9fbc8d6cb9916bc3a98eb
Sergi Mayol <sergi2808@gmail.com>
2022-11-22 12:25:05 +0100

    Update SemanticAnalyzer.java

--- src/semantic/SemanticAnalyzer.java
+++ src/semantic/SemanticAnalyzer.java
@@ -1,7 +1,13 @@
 package semantic;
 
 import symbol_table.SymbolTable;
+import symbol_table.Symbol;
+import symbol_table.Symbol.Type;
+import symbol_table.Symbol.SubType;
+import utils.Phase;
 import errors.ErrorHandler;
+import core.Instr;
+import core.ReturnNode;
 import errors.ErrorCode;
 
 public class SemanticAnalyzer {
@@ -32,9 +38,25 @@ public class SemanticAnalyzer {
         return false;
     }
 
-    public boolean checkReturn() {
-        // !TODO: Implement this method
-        return false;
+    /**
+     * Checks if the return type of a function is compatible with the return type.
+     * 
+     * @param id        - The name of the function.
+     * @param subType   - The return type of the function.
+     * @param returnExp - The return expression of the function.
+     * @param line      - The line number of the return statement.
+     * 
+     * @return true if the return type is compatible, false otherwise
+     */
+    public boolean checkReturn(String id, SubType subType, ReturnNode returnExp, int line) {
+        // Check if the return type of the function is the same as the return type of
+        // the expression. If not, add an error to the error handler
+        SubType functionSubType = this.symbolTable.getSymbol(id).getSubType();
+        if (functionSubType != subType) {
+            ErrorHandler.addError(ErrorCode.INCOMPATIBLE_TYPES, line, Phase.SEMANTIC);
+            return false;
+        }
+        return true;
     }
 
     public boolean checkFunctionCall() {
@@ -47,9 +69,21 @@ public class SemanticAnalyzer {
         return false;
     }
 
-    public boolean checkIdentifier() {
-        // !TODO: Implement this method
-        return false;
+    /**
+     * This method checks if the ID is declared in the current scope.
+     * 
+     * @param id   - The ID to check
+     * @param line - The line where the ID is declared
+     * 
+     * @return true if the ID is declared in the current scope, false otherwise.
+     */
+    public boolean checkIdentifier(String id, int line) {
+        Symbol symbol = this.symbolTable.getSymbol(id);
+        if (symbol == null) {
+            ErrorHandler.addError(ErrorCode.UNDECLARED_IDENTIFIER, line, Phase.SEMANTIC);
+            return false;
+        }
+        return true;
     }
 
 }
