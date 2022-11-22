64e96d019e5d3d973ed71e216eb4ff38a97ca138
Sergi Mayol <sergi2808@gmail.com>
2022-11-22 11:30:58 +0100

    Estructura inicial analizador semántico

--- src/semantic/SemanticAnalyzer.java
+++ src/semantic/SemanticAnalyzer.java
@@ -1,5 +1,55 @@
 package semantic;
 
-public class Semantic {
+import symbol_table.SymbolTable;
+import errors.ErrorHandler;
+import errors.ErrorCode;
+
+public class SemanticAnalyzer {
+
+    private SymbolTable symbolTable;
+
+    public SemanticAnalyzer(SymbolTable symbolTable) {
+        this.symbolTable = symbolTable;
+    }
+
+    public boolean checkVariableDeclaration() {
+        // !TODO: Implement this method
+        return true;
+    }
+
+    public boolean checkFunctionDeclaration() {
+        // !TODO: Implement this method
+        return true;
+    }
+
+    public boolean checkExpression() {
+        // !TODO: Implement this method
+        return true;
+    }
+
+    public boolean checkAssignment() {
+        // !TODO: Implement this method
+        return true;
+    }
+
+    public boolean checkReturn() {
+        // !TODO: Implement this method
+        return true;
+    }
+
+    public boolean checkFunctionCall() {
+        // !TODO: Implement this method
+        return true;
+    }
+
+    public boolean checkInstruction() {
+        // !TODO: Implement this method
+        return true;
+    }
+
+    public boolean checkIdentifier() {
+        // !TODO: Implement this method
+        return true;
+    }
 
 }
